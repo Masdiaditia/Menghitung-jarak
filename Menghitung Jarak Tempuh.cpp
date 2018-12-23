d56916eada3a92495a900b07d813ea29bfd3ba16
Masdi Aditia <45525790+Masdiaditia@users.noreply.github.com>
2018-12-23 16:11:25 +0700

    Add files via upload

---
+++ Menghitung Jarak Tempuh.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    float V,S,T;
+    printf("masukkan kecepatan mobil =");
+    scanf("%f",&V);
+    printf("masukkan waktu tempuh =");
+    scanf ("%f",&T);
+    S = V*T;
+    printf("maka jarak yang di tempuh adalah =%f",S);
+    return 0;
+
+
+}
